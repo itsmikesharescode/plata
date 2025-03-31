764d78c91aff6104e6bd9bf2fc6c49e0311f4d19
itsmikesharescode <eviotamikejohnb@gmail.com>
2025-03-31 19:01:45 +0800

    add user info

--- src/lib/components/general/operation-sidebar/components/header.svelte
+++ src/lib/components/general/operation-sidebar/components/header.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <script lang="ts">
+	const user = $derived(page.data.user);
 </script>
 
 <header class="flex items-center gap-2">
@@ -10,7 +11,9 @@
 		class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
 	></div>
 	<div class="grid flex-1 text-left text-sm leading-tight">
-		<span class="truncate text-sm font-semibold">Fernando </span>
-		<span class="truncate text-xs font-bold text-muted-foreground">ADMINISTRATOR</span>
+		<span class="truncate text-sm font-semibold">{user?.user_metadata.fullname}</span>
+		<span class="truncate text-xs font-bold text-muted-foreground">
+			{user?.user_metadata.role === 'admin' ? 'ADMINISTRATOR' : 'ChairPerson'}
+		</span>
 	</div>
 </header>
