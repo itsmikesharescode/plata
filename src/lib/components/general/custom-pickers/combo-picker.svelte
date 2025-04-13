2f86ed029d1cbbf4147071a348ef5492930533e1
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-04-14 02:21:17 +0800

    add: wips

---
+++ src/lib/components/general/custom-pickers/combo-picker.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import Check from '@lucide/svelte/icons/check';
+	import ChevronsUpDown from '@lucide/svelte/icons/chevrons-up-down';
+	import { tick } from 'svelte';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+	import type { Snippet } from 'svelte';
+
+	//TODO: continue implement a dynamic combo picker
+
+	interface Props {
+		selections: {
+			id: string;
+			label: string;
+			value: string;
+		}[];
+		nameHolder?: string;
+		childLoop: Snippet<[{ selectedItem: Props['selections'][number] }]>;
+	}
+
+	let { selections, nameHolder = 'department', childLoop }: Props = $props();
+
+	let open = $state(false);
+	let value = $state('');
+	let triggerRef = $state<HTMLButtonElement>(null!);
+
+	const selectedValue = $derived(selections.find((f) => f.value === value));
+
+	function closeAndFocusTrigger() {
+		open = false;
+		tick().then(() => {
+			triggerRef.focus();
+		});
+	}
+</script>
+
+<Popover.Root bind:open>
+	<Popover.Trigger bind:ref={triggerRef}>
+		{#snippet child({ props })}
+			<Button
+				variant="outline"
+				class="w-full justify-between"
+				{...props}
+				role="combobox"
+				aria-expanded={open}
+			>
+				{selectedValue?.label || `Select a ${nameHolder}`}
+				<ChevronsUpDown class="ml-2 size-4 shrink-0 opacity-50" />
+			</Button>
+		{/snippet}
+	</Popover.Trigger>
+	<Popover.Content class="w-full p-0">
+		<Command.Root>
+			<Command.Input placeholder={`Search ${nameHolder}...`} />
+			<Command.List>
+				<Command.Empty>No {nameHolder} found.</Command.Empty>
+				<Command.Group>
+					{#each selections as selection}
+						<Command.Item
+							value={selection.value}
+							onSelect={() => {
+								value = selection.value;
+								closeAndFocusTrigger();
+							}}
+						>
+							<Check class={cn('mr-2 size-4', value !== selection.value && 'text-transparent')} />
+							{@render childLoop({ selectedItem: selection })}
+						</Command.Item>
+					{/each}
+				</Command.Group>
+			</Command.List>
+		</Command.Root>
+	</Popover.Content>
+</Popover.Root>
--- src/routes/operation/(management)/schedules/+page.svelte
+++ src/routes/operation/(management)/schedules/+page.svelte
@@ -6,16 +6,54 @@
 	import { columns } from './(components)/(table)/column';
 	import Printing from './(components)/(toolbars)/(printing)/printing.svelte';
 	import { initPrintingState } from './(components)/(toolbars)/(printing)/state.svelte';
-
+	import SimplePicker from '$lib/components/general/custom-pickers/simple-picker.svelte';
+	import { page } from '$app/state';
+	import type { DepartmentDropdown } from '../../+layout.svelte';
+	import ComboPicker from '$lib/components/general/custom-pickers/combo-picker.svelte';
 	const { data } = $props();
 
 	initPrintingState();
+
+	const departmentsDropdown = $derived(page.data.departmentsDropdown) as DepartmentDropdown;
+
+	let selectedDepartmentId = $state('');
 </script>
 
 <main class="flex flex-col gap-4 p-4">
-	<section class="flex items-center gap-4">
-		<CreateSchedule createScheduleForm={data.createScheduleForm} />
-		<Printing />
+	<section class="flex flex-wrap items-center justify-between gap-4">
+		<div class="">
+			<CreateSchedule createScheduleForm={data.createScheduleForm} />
+			<Printing />
+		</div>
+
+		<div class="grid grid-cols-[auto_auto] items-center gap-4">
+			<span class="w-full text-sm font-medium">Filter by:</span>
+			<ComboPicker
+				selections={departmentsDropdown?.map((v) => ({
+					id: v.id,
+					label: v.department_code,
+					value: JSON.stringify({
+						department_name: v.department_name,
+						department_color: v.department_color
+					})
+				})) ?? []}
+			>
+				{#snippet childLoop({ selectedItem })}
+					<div class="grid grid-cols-[auto_1fr] items-center gap-2">
+						<div
+							class="size-5 rounded-full"
+							style="background-color: {JSON.parse(selectedItem.value).department_color}"
+						></div>
+						<div class="flex flex-col">
+							<span class="">{selectedItem.label}</span>
+							<span class="text-xs text-muted-foreground"
+								>{JSON.parse(selectedItem.value).department_name}</span
+							>
+						</div>
+					</div>
+				{/snippet}
+			</ComboPicker>
+		</div>
 	</section>
 	<section>
 		<CustomTable {columns} data={data.schedules ?? []} />
